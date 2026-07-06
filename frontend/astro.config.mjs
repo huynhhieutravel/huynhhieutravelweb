6bde8decdf24a0d9c5dd1cf00b02885ed1d3d264
Huỳnh Trọng Hiếu <huynhtronghieu@Mac.lan>
2026-07-06 19:05:20 +0700

    Fix DB bindings in static pages and update astro config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -7,9 +7,10 @@ const isDev = process.env.npm_lifecycle_event === 'dev';
 
 export default defineConfig({
   output: 'server',
-  redirects: {
-    '/home': '/'
+  build: {
+    format: 'file'
   },
+  trailingSlash: 'ignore',
   adapter: isDev ? undefined : cloudflare({
     platformProxy: {
       enabled: true
